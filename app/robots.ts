ea51a5aaaaf685048d164fa76ce5b354e57619f5
riansyahp367-gif <riansyahp367@gmail.com>
2026-09-13 14:32:57 +0700

    Fix production robots sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,13 @@
 import type { MetadataRoute } from "next";
 
+const siteUrl = "https://saladbuahsenja-web.vercel.app";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "http://localhost:3000/sitemap.xml",
+    sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
